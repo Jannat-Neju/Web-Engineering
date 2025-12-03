c3b90e7b502a93f8f7efa055843c96f87be7d6e0
Jannat-Neju <jannatneju200014@gmail.com>
2025-12-03 09:51:22 +0600

    Updated project files

--- green_matcha_website/config.php
+++ green_matcha_website/config.php
@@ -1,18 +1,15 @@
 <?php
 
-// Load environment variables
-$db_host = getenv('DB_HOST');
-$db_user = getenv('DB_USER');
-$db_pass = getenv('DB_PASS');
-$db_name = getenv('DB_NAME');
-$db_port = getenv('DB_PORT');
+$db_host = "localhost";
+$db_user = "root";
+$db_pass = ""; 
+$db_name = "shop_db";
+$db_port = 3306;
 
-// Connect to MySQL
 $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name, $db_port);
 
-// Debug if connection fails
 if (!$conn) {
-    die("Database connection failed: " . mysqli_connect_error());
+    die("Connection failed: " . mysqli_connect_error());
 }
 
 ?>
