077448b40c6f00916a3b6ccc5e6a07a70ae54d4a
Jannat-Neju <jannatneju200014@gmail.com>
2025-12-04 06:14:23 +0600

    Updated project files

--- green_matcha_website/config.php
+++ green_matcha_website/config.php
@@ -1,10 +1,10 @@
 <?php
 
-$db_host = "localhost";
-$db_user = "root";
-$db_pass = ""; 
-$db_name = "shop_db";
-$db_port = 3306;
+$db_host = getenv('DB_HOST');
+$db_user = getenv('DB_USER');
+$db_pass = getenv('DB_PASS');
+$db_name = getenv('DB_NAME');
+$db_port = getenv('DB_PORT') ?: 3306;
 
 $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name, $db_port);
 
